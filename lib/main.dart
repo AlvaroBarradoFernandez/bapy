60b23167ac41dc5e7a07c0f0fb2973abaa3f41a2
Alvaro Barrado Fernandez <barradoa3648@gmail.com>
2019-10-15 20:02:24 +0200

    color

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,193 @@ class ChartPage extends StatefulWidget {
   ChartPageState createState() => ChartPageState();
 }
 
+int auxpoint = 0;
 int points = 0;
 int points2 = 0;
 
 class ChartPageState extends State<ChartPage> {
+
+  void updatePoint(int points) {
+
+    setState(() {
+      auxpoint = points;
+    });
+  }
+  void updatePoint2(int points2) {
+
+    setState(() {
+      auxpoint = points2;
+    });
+  }
   @override
   Widget build(BuildContext context) {
+    Color mColor = Colors.cyan;
+
+    Future<void> _neverSatisfied(context) async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Has perdido :('),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text(points.toString()),
+                  Text('Tu puntuación final es de ' + points2.toString()),
+                  Text('You\’re like me. I’m never satisfied.'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cerrar'),
+                onPressed: () {
+                  points2 = 0;
+                  Navigator.of(context).pop();
+                },
+              ),
+              MaterialButton(
+                child: Text('hey'),
+                onPressed: () {
+                  updatePoint(points);
+                    updatePoint2(points2);
+                    print('aux');
+                    print(auxpoint);
+                    points = auxpoint;
+                  print('hey');
+                  print(points);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    var value = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      '11',
+      '12',
+      '13',
+      '14',
+      '15',
+      '16',
+      '17',
+      '18',
+      '19',
+      '20',
+      '21',
+      '22',
+      '23',
+      '24',
+      '25'
+    ];
+
+    List<int> recuadro = [
+      1,
+      1,
+      1,
+      9999,
+      1,
+      1,
+      1,
+      9999,
+      9999,
+      9999,
+      9999,
+      0,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+
+
+    ];
+
+    Widget myPlay = new GridView.builder(
+
+      scrollDirection: Axis.vertical,
+      itemCount: value.length,
+      shrinkWrap: true,
+      gridDelegate:
+      new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+      itemBuilder: (BuildContext context, int index) {
+final _random = new Random(index);
+
+        //recuadro.shuffle(Random(recuadro.elementAt(index)));
+        int rng = recuadro[_random.nextInt(recuadro.length)];
+        //List<int> mList = new List<int>();
+        //mList.add(rng);
+        //mList.shuffle();
+        //int rng2 = mList.elementAt(index);
+        //shuffle(new Random());
+
+        var colorr = Colors.cyan;
+        var colorrr = Colors.black;
+        return new GestureDetector(
+          child: new Card(
+            color: colorr,
+            child: new Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(120.0))),
+              alignment: Alignment.center,
+              child: new Column(
+                  children: <Widget>[Container(child: Text(rng.toString()))]),
+            ),
+          ),
+          onTap: () {
+            points2 = auxpoint;
+            updatePoint(points);
+            updatePoint2(points2);
+            print('points2');
+            print(points2);
+            if (rng == 0) {
+              points = 0;
+              updatePoint(points);
+              _neverSatisfied(context);
+              mColor = colorr;
+              print('points');
+              print(points);
+              print('points2');
+              print(points2);
+              new Column(
+                  children: <Widget>[Container(child: Text(rng.toString()))]);
+            } else {
+              points2 += rng;
+              points += rng;
+              updatePoint2(points2);
+              updatePoint(points);
+              print('points2222');
+              print(points2);
+
+
+            }
+          },
+        );
+      },
+    );
+
     var screenSize = MediaQuery.of(context).size;
     var width = screenSize.width;
     var height = screenSize.height;
@@ -44,12 +225,11 @@ class ChartPageState extends State<ChartPage> {
                               offset: Offset(0.7, 0.7),
                               blurRadius: 10)
                         ]),
-                    child: Center( child: Text(
-                      points.toString(),
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.8)),
+                    child: Center( child: Text('$auxpoint',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1.8)),
                     )),
                 Padding(
                   padding: EdgeInsets.only(top: 10),
@@ -87,163 +267,9 @@ class ChartPageState extends State<ChartPage> {
             myPlay,
           ]),
         ));
+
+
   }
+
 }
 
-Color mColor = Colors.cyan;
-
-Future<void> _neverSatisfied(context) async {
-  return showDialog<void>(
-    context: context,
-    barrierDismissible: false, // user must tap button!
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Has perdido :('),
-        content: SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-              Text(points.toString()),
-              Text('Tu puntuación final es de ' + points2.toString()),
-              Text('You\’re like me. I’m never satisfied.'),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          FlatButton(
-            child: Text('Cerrar'),
-            onPressed: () {
-              points2 = 0;
-              Navigator.of(context).pop();
-            },
-          ),
-          MaterialButton(
-            child: Text('hey'),
-            onPressed: () {
-              print('hey');
-            },
-          ),
-        ],
-      );
-    },
-  );
-}
-
-var value = [
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-  '14',
-  '15',
-  '16',
-  '17',
-  '18',
-  '19',
-  '20',
-  '21',
-  '22',
-  '23',
-  '24',
-  '25',
-  '26',
-  '27',
-  '28',
-  '29',
-  '30',
-  '31',
-  '32',
-  '33',
-  '34',
-  '35',
-  '36'
-];
-
-var recuadro = [
-  1,
-  1,
-  1,
-  9999,
-  1,
-  1,
-  1,
-  9999,
-  9999,
-  9999,
-  9999,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1
-];
-
-Widget myPlay = new GridView.builder(
-
-  scrollDirection: Axis.vertical,
-  itemCount: value.length,
-  shrinkWrap: true,
-  gridDelegate:
-      new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 6),
-  itemBuilder: (BuildContext context, int index) {
-
-    var rng = recuadro.shuffle(new Random());
-    var rng2 = int.parse(rng);
-    var colorr = Colors.cyan;
-    var colorrr = Colors.black;
-    return new GestureDetector(
-      child: new Card(
-        color: mColor,
-        child: new Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(120.0))),
-          alignment: Alignment.center,
-          child: new Column(
-              children: <Widget>[Container(child: Text(rng2.toString()))]),
-        ),
-      ),
-      onTap: () {
-        if (rng2 == 0) {
-          points = 0;
-          _neverSatisfied(context);
-          mColor = Colors.black;
-          new Column(
-              children: <Widget>[Container(child: Text(rng2.toString()))]);
-        } else {
-          print(rng2);
-          points += rng2;
-          print(points);
-          points2 += rng2;
-        }
-      },
-    );
-  },
-);
